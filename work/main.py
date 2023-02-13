abe15df21a00be32ead7286961b90e53c305380e
Junjie Yang <yangjunjie0320@gmail.com>
2023-02-12 17:21:21 -0800

    update

--- work/main.py
+++ work/main.py
@@ -23,8 +23,8 @@ def main(m, basis, dir_path, h5_path, x_list):
 if __name__ == "__main__":
     dir_path = f"./data/"
     if os.path.exists(dir_path):
-        os.remove(dir_path)
-        
+        os.removedirs(dir_path)
+
     m        = "h2"
     basis    = "sto-3g"
     h5_path  = os.path.join(dir_path, f"{m}-{basis}.h5")
