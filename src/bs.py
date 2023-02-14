39019a1fb10e4e3f48065dc9bae29b97a2aa0afe
Junjie Yang <yangjunjie0320@gmail.com>
2023-02-14 12:09:39 -0800

    update

--- src/bs.py
+++ src/bs.py
@@ -169,6 +169,8 @@ def solve_uhf_noci(v_bs_uhf_list, hv_bs_uhf_list, ene_bs_uhf_list, tol=1e-8):
     v_dot_v  = numpy.einsum('Iab,Jab->IJ', v_bs_uhf_list, v_bs_uhf_list)
     v_dot_hv = numpy.einsum('Iab,Jab->IJ', v_bs_uhf_list, hv_bs_uhf_list)
 
+    dump_rec(stdout, v_dot_v)
+
     ene_err = numpy.diag(v_dot_hv) / numpy.diag(v_dot_v) - ene_bs_uhf_list
     ene_err = numpy.linalg.norm(ene_err)
     if not ene_err < tol:
