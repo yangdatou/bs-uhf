c421ae3c162ea6c71b27326fd8708f22fd67b825
Junjie Yang <yangjunjie0320@gmail.com>
2023-02-15 13:45:21 -0800

    update

--- work/main.py
+++ work/main.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     dir_path = f"./data/"
     m        = "h2"
     basis    = "sto-3g"
-    h5_path  = os.path.join(dir_path, f"{m}-{basis}-scf.h5")
-    x_list   = numpy.linspace(0.5, 3.0, 41)
-    main(m, basis, dir_path, h5_path, x_list, is_scf=True)
+    # h5_path  = os.path.join(dir_path, f"{m}-{basis}-scf.h5")
+    # x_list   = numpy.linspace(0.5, 3.0, 41)
+    # main(m, basis, dir_path, h5_path, x_list, is_scf=True)
 
     # basis    = "cc-pvdz"
     # h5_path  = os.path.join(dir_path, f"{m}-{basis}.h5")
@@ -56,8 +56,8 @@ if __name__ == "__main__":
     # x_list   = numpy.linspace(0.5, 3.0, 41)
     # main(m, basis, dir_path, h5_path, x_list)
 
-    # m       = "n2"
-    # basis    = "sto-3g"
-    # h5_path = os.path.join(dir_path, f"{m}-{basis}.h5")
-    # x_list  = numpy.linspace(0.8, 3.0, 41)
-    # main(m, basis, dir_path, h5_path, x_list)
+    m       = "n2"
+    basis   = "sto-3g"
+    h5_path = os.path.join(dir_path, f"{m}-{basis}-scf.h5")
+    x_list  = numpy.linspace(0.8, 3.0, 41)
+    main(m, basis, dir_path, h5_path, x_list, is_scf=True)
